a5adcc272a8feb128d3966dc8cee65da4bb2f310
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2020-09-11 00:12:05 -0700

    Working terraform for deploying a Solr Cloud using the Solr operator
    Given an existing kubeconfig, this first draft Terraform:
    - Helm installs ingress-nginx
    - Helm installs the zookeeper-operator
    - Helm installs the solr-operator
    - Helm installs a local solr-crd Helm chart to get a SolrCloud instance running
    NOTE: a local valid kubeconfig is assumed to be in place! A future rev will take it as an input

---
+++ services/terraform-solr/main.tf
@@ -0,0 +1,101 @@
+# ===
+# Vars
+variable "cluster_id" {
+  description = "The cluster ID to target from the passed kubeconfig"
+  default = "docker-desktop"
+}
+
+variable "ingress_base_domain" {
+  description = "The base domain to expose Solr on, eg *.(ingress-base-domain)"
+  default = "ing.local.domain"
+}
+# ===
+# Providers
+provider "kubernetes" {
+  config_context_cluster   = "docker-desktop"
+}
+
+provider "helm" {
+  kubernetes {
+    config_context_cluster   = var.cluster_id
+  }
+}
+
+# ===
+# k8s namespaces
+resource "kubernetes_namespace" "nginx-ingress" {
+  metadata {
+    name = "nginx-ingress"
+  }
+}
+
+resource "kubernetes_namespace" "solr" {
+  metadata {
+    name = "solr"
+  }
+}
+
+# ===
+# Helm charts
+
+# Ingress should be assumed of whatever k8s provider is passed in, but for now...
+resource "helm_release" "ingress" {
+  name = "ingress"
+  chart = "nginx-ingress"
+  repository = "https://helm.nginx.com/stable"
+  namespace = "nginx-ingress"
+  cleanup_on_fail = "true"
+  atomic = "true"
+
+  provisioner "local-exec" {
+    command = "helm --kube-context ${var.cluster_id} test -n ${self.namespace} ${self.name}"
+  }
+}
+
+resource "helm_release" "zookeeper" {
+  name = "zookeeper"
+  chart = "zookeeper-operator"
+  repository = "https://charts.pravega.io/"
+  namespace = "solr"
+  cleanup_on_fail = "true"
+  atomic = "true"
+
+  provisioner "local-exec" {
+    command = "helm --kube-context ${var.cluster_id} test -n ${self.namespace} ${self.name}"
+  }
+}
+
+resource "helm_release" "solr" {
+  name = "solr"
+  chart = "solr-operator"
+  repository = "https://bloomberg.github.io/solr-operator/charts"
+  namespace = "solr"
+  cleanup_on_fail = "true"
+  atomic = "true"
+
+  set {
+    name  = "ingressBaseDomain"
+    value = var.ingress_base_domain
+  }
+
+  provisioner "local-exec" {
+    command = "helm --kube-context ${var.cluster_id} test -n ${self.namespace} ${self.name}"
+  }
+}
+
+resource "helm_release" "solrcloud" {
+  name = "example"
+  chart = "solr-crd"
+  cleanup_on_fail = "true"
+  atomic = "true"
+
+  # set {
+  #   name  = "ingressBaseDomain"
+  #   value = var.ingress_base_domain
+  # }
+
+  # provisioner "local-exec" {
+  #   command = "helm --kube-context ${var.cluster_id} test -n ${self.namespace} ${self.name}"
+  # }
+}
+
