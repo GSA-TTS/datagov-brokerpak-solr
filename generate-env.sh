c4961894c903f507e90e5a8843e52b6b76973872
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-02-18 22:53:54 -0800

    Rename the env vars to be about Solr specifically
    This way a different set of credentials can be used per service

--- generate-env.sh
+++ generate-env.sh
@@ -3,29 +3,29 @@
 set -e
 
 CURRENT_CONTEXT=$(kubectl config current-context)
-K8S_CLUSTER_CA_CERTIFICATE=$(kubectl config view --raw -o json | jq -r '.clusters[]| select(.name | contains("'${CURRENT_CONTEXT}'"))  .cluster["certificate-authority-data"]')
-K8S_TOKEN=$(kubectl get secret $( kubectl get serviceaccount default -n default -o json | jq -r '.secrets[0].name' ) -n default -o json | jq -r .data.token)
+SOLR_CLUSTER_CA_CERTIFICATE=$(kubectl config view --raw -o json | jq -r '.clusters[]| select(.name | contains("'${CURRENT_CONTEXT}'"))  .cluster["certificate-authority-data"]')
+SOLR_TOKEN=$(kubectl get secret $( kubectl get serviceaccount default -n default -o json | jq -r '.secrets[0].name' ) -n default -o json | jq -r .data.token)
 
 # We need the Docker-internal control plane URL to be resolved for the CSB
 # when running in a container
-K8S_DOCKER_SERVER=$(kind get kubeconfig --internal --name=$(kind get clusters | grep datagov-broker-test) | grep server | cut -d ' ' -f 6-)
+SOLR_DOCKER_SERVER=$(kind get kubeconfig --internal --name=$(kind get clusters | grep datagov-broker-test) | grep server | cut -d ' ' -f 6-)
 
 # We need the localhost control plan URL to be used for direct access when we
 # work outside the CSB
-K8S_LOCALHOST_SERVER=$(kind get kubeconfig --name=$(kind get clusters | grep datagov-broker-test) | grep server | cut -d ' ' -f 6-)
+SOLR_LOCALHOST_SERVER=$(kind get kubeconfig --name=$(kind get clusters | grep datagov-broker-test) | grep server | cut -d ' ' -f 6-)
 
 # Generate the environment variables needed for configuring the CSB running in Docker
-echo K8S_SERVER=${K8S_DOCKER_SERVER} > .env
-echo K8S_TOKEN=${K8S_TOKEN} >> .env
-echo K8S_CLUSTER_CA_CERTIFICATE=${K8S_CLUSTER_CA_CERTIFICATE} >> .env
-echo K8S_NAMESPACE=default >> .env
-echo K8S_DOMAIN_NAME=ing.local.domain >> .env
+echo SOLR_SERVER=${SOLR_DOCKER_SERVER} > .env
+echo SOLR_TOKEN=${SOLR_TOKEN} >> .env
+echo SOLR_CLUSTER_CA_CERTIFICATE=${SOLR_CLUSTER_CA_CERTIFICATE} >> .env
+echo SOLR_NAMESPACE=default >> .env
+echo SOLR_DOMAIN_NAME=ing.local.domain >> .env
 
 # Generate terraform.tfvars needed for mucking about directly with terraform/provision
 cat > terraform/provision/terraform.tfvars << HEREDOC
-server="${K8S_LOCALHOST_SERVER}"
-token="${K8S_TOKEN}"
-cluster_ca_certificate="${K8S_CLUSTER_CA_CERTIFICATE}"
+server="${SOLR_LOCALHOST_SERVER}"
+token="${SOLR_TOKEN}"
+cluster_ca_certificate="${SOLR_CLUSTER_CA_CERTIFICATE}"
 namespace="default"
 domain_name="ing.local.domain"
 replicas=3
