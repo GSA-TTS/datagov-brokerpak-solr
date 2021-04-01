6d36f1353849ac27af006d7c6243d6fd2284fa66
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-03-31 21:51:18 -0700

    terraform fmt

--- terraform/bind/outputs.tf
+++ terraform/bind/outputs.tf
@@ -4,8 +4,8 @@ output "uri" {
     "http", # We need to derive this programmatically from the kubernetes_ingress in future. 
     random_uuid.username.result,
     random_password.password.result,
-    data.kubernetes_ingress.solrcloud-ingress.spec[0].rule[0].host)
+  data.kubernetes_ingress.solrcloud-ingress.spec[0].rule[0].host)
 }
-output "domain"  { value = data.kubernetes_ingress.solrcloud-ingress.spec[0].rule[0].host }
+output "domain" { value = data.kubernetes_ingress.solrcloud-ingress.spec[0].rule[0].host }
 output "username" { value = random_uuid.username.result }
 output "password" { value = random_password.password.result }
