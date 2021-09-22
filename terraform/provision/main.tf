516323e2e7daf8960cd198933cafdb29dac6642e
Nicholas Kumia <nicholas.kumia@gsa.gov>
2021-09-21 22:26:24 -0400

    new: deallocate credentials from nginx login; create framework for supporting credentials for solr operator; right now, only the bootstrapped (or pregenerated) credentials work; custom credentials are yet to be functional

--- terraform/provision/main.tf
+++ terraform/provision/main.tf
@@ -13,6 +13,121 @@ resource "kubernetes_secret" "client_creds" {
   type = "Opaque"
 }
 
+
+resource "kubernetes_secret" "security_json" {
+  metadata {
+    # TODO: This should replace the bootstrapped security.json
+    name      = "${local.cloud_name}-security-bootstrap"
+    # name      = "${local.cloud_name}-solrcloud-security-bootstrap"
+    namespace = var.namespace
+    labels    = {
+      "app.kubernetes.io/instance"    = "${local.cloud_name}"
+      "app.kubernetes.io/managed-by"  = "data.gov"
+      "app.kubernetes.io/name"        = "solr"
+      "app.kubernetes.io/version"     = "8.9.0"
+      "helm.sh/chart"                 = "solr-0.4.0"
+      "solr-cloud"                    = "${local.cloud_name}"
+    }
+  }
+
+  data = {
+    # TODO: These should be created via bind, but provision should at least restrict access to everyone
+    # "admina" = base64encode("password")
+    # "k8sop" = base64encode("password")
+    # 
+    # The "security.json" attribute doesn't register with solr and doesn't restrict users, needs to be fixed
+    "security.json" = base64encode(tostring(jsonencode(
+    {
+  "authentication": {
+    "blockUnknown": true,
+    "class": "solr.BasicAuthPlugin",
+    "credentials": {
+      "admina": base64encode("password"),
+      "k8sop": base64encode("password")
+    },
+    "realm": "Solr Basic Auth",
+    "forwardCredentials": false
+  },
+  "authorization": {
+    "class": "solr.RuleBasedAuthorizationPlugin",
+    "user-role": {
+      "admina": [ "admin", "k8s" ],
+      "k8s-op": [ "admin", "k8s" ]
+    },
+    "permissions": [
+      {
+        "name": "k8s-probe-0",
+        "role": null,
+        "collection": null,
+        "path": "/admin/info/system"
+      },
+      {
+        "name": "k8s-status",
+        "role": "k8s",
+        "collection": null,
+        "path": "/admin/collections"
+      },
+      {
+        "name": "k8s-metrics",
+        "role": "k8s",
+        "collection": null,
+        "path": "/admin/metrics"
+      },
+      {
+         "name": "k8s-zk",
+         "role":"k8s",
+         "collection": null,
+         "path":"/admin/zookeeper/status"
+      },
+      {
+        "name": "k8s-ping",
+        "role": "k8s",
+        "collection": "*",
+        "path": "/admin/ping"
+      },
+      {
+        "name": "read",
+        "role": [ "admin", "users" ]
+      },
+      {
+        "name": "update",
+        "role": [ "admin" ]
+      },
+      {
+        "name": "security-read",
+        "role": [ "admin" ]
+      },
+      {
+        "name": "security-edit",
+        "role": [ "admin" ]
+      },
+      {
+        "name": "all",
+        "role": [ "admin" ]
+      }
+    ]
+  }})))
+  }
+
+  type = "Opaque"
+}
+
+resource "kubernetes_secret" "operator_auth" {
+  metadata {
+    # TODO: This should replace the bootstrapped basic-auth to communicate with solr operator
+    name      = "${local.cloud_name}-basic-auth"
+    # name      = "${local.cloud_name}-solrcloud-basic-auth"
+    namespace = var.namespace
+  }
+
+  data = {
+    username = base64encode("k8sop")
+    password = base64encode("password")
+  }
+
+  type = "kubernetes.io/basic-auth"
+}
+
 data "template_file" "kubeconfig" {
   template = <<-EOF
     apiVersion: v1
@@ -55,14 +170,16 @@ resource "helm_release" "solrcloud" {
       "dataStorage.type"                                                        = "ephemeral"
       "image.repository"                                                        = var.solrImageRepo                               # Which Docker repo to use for pulling the Solr image (defaults to docker.io/solr)
       "image.tag"                                                               = var.solrImageTag                                # Which version of Solr to use (specify a tag from the official Solr images at https://hub.docker.com/_/solr)
-      "ingressOptions.annotations.nginx\\.ingress\\.kubernetes\\.io/auth-type"  = "basic"
-      "ingressOptions.annotations.nginx\\.ingress\\.kubernetes\\.io/auth-realm" = "Authentication Required - admin"
-      "ingressOptions.annotations.nginx\\.ingress\\.kubernetes\\.io/auth-secret"= kubernetes_secret.client_creds.metadata[0].name # The name of the secret to be used for authentication
+      # "ingressOptions.annotations.nginx\\.ingress\\.kubernetes\\.io/auth-type"  = "basic"
+      # "ingressOptions.annotations.nginx\\.ingress\\.kubernetes\\.io/auth-realm" = "Authentication Required - admin"
+      # "ingressOptions.annotations.nginx\\.ingress\\.kubernetes\\.io/auth-secret"= kubernetes_secret.client_creds.metadata[0].name # The name of the secret to be used for authentication
       "podOptions.resources.requests.memory"                                    = var.solrMem # How much memory to request from the scheduler
       "podOptions.resources.requests.cpu"                                       = var.solrCpu # How much vCPU to request from the scheduler
       "replicas"                                                                = var.replicas                                    # How many replicas you want
       "solrOptions.javaMemory"                                                  = var.solrJavaMem                                 # How much memory to give each replica
-      # "solrOptions.security.basicAuthSecret"                                    = kubernetes_secret.client_creds.metadata[0].name # The name of the secret to be used for authentication
+      "solrOptions.security.authenticationType"                                 = "Basic"
+      # TODO: Create a secret unique to each provisioned instance (this may not be necessary)
+      # "solrOptions.security.basicAuthSecret"                                    = kubernetes_secret.operator_auth.metadata[0].name
     }
     content {
       name  = set.key
