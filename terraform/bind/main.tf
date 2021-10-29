f8b5c5ca25466a16b348d8ad7a4ef53fb44d4c58
Nicholas Kumia <nicholas.kumia@gsa.gov>
2021-10-28 22:10:15 -0400

    upgrade: terraform version and brokerpak image
    Mainly, don't refer to directly to other variables in destroy-time provisioner

--- terraform/bind/main.tf
+++ terraform/bind/main.tf
@@ -46,12 +46,22 @@ locals {
 #   https://superuser.com/questions/272265/getting-curl-to-output-http-status-code#comment1567079_442395
 
 resource "null_resource" "manage_solr_user" {
+  # The method used below for referencing external resources in a destroy
+  # provisioner via triggers comes from
+  # https://github.com/hashicorp/terraform/issues/23679#issuecomment-886020367
+  triggers = {
+    kubeconfig = base64encode(data.template_file.kubeconfig.rendered)
+    admin_password = local.admin_password
+    delete_user_json = local.delete_user_json
+    clear_role_json = local.clear_role_json
+    cloud_name = local.cloud_name
+  }
 
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     environment = {
-      KUBECONFIG         = base64encode(data.template_file.kubeconfig.rendered)
-      ADMIN_PASSWORD     = local.admin_password
+      KUBECONFIG         = self.triggers.kubeconfig
+      ADMIN_PASSWORD     = self.triggers.admin_password
       GENERATED_PASSWORD = random_password.password.result
       CREATE_USER_JSON   = local.create_user_json
       SET_ROLE_JSON      = local.set_role_json
@@ -65,14 +75,14 @@ resource "null_resource" "manage_solr_user" {
         -o /dev/null \
         -w "%%{http_code}\n" \
         --user admin:$${ADMIN_PASSWORD} \
-        'http://${local.cloud_name}-solrcloud-common/solr/admin/authentication' \
+        'http://${self.triggers.cloud_name}-solrcloud-common/solr/admin/authentication' \
         -H 'Content-type:application/json' --data "$CREATE_USER_JSON"
       kubectl --kubeconfig <(echo $KUBECONFIG | base64 -d) run temp2 --image=curlimages/curl --  \
         -s -f -L \
         -o /dev/null \
         -w "%%{http_code}\n" \
         --user admin:$${ADMIN_PASSWORD} \
-        'http://${local.cloud_name}-solrcloud-common/solr/admin/authorization' \
+        'http://${self.triggers.cloud_name}-solrcloud-common/solr/admin/authorization' \
         -H 'Content-type:application/json' --data "$SET_ROLE_JSON"
       sleep 10
       kubectl --kubeconfig <(echo $KUBECONFIG | base64 -d) delete pod temp2
@@ -84,10 +94,10 @@ resource "null_resource" "manage_solr_user" {
     interpreter = ["/bin/bash", "-c"]
     when        = destroy
     environment = {
-      KUBECONFIG       = base64encode(data.template_file.kubeconfig.rendered)
-      ADMIN_PASSWORD   = local.admin_password
-      DELETE_USER_JSON = local.delete_user_json
-      CLEAR_ROLE_JSON  = local.clear_role_json
+      KUBECONFIG       = self.triggers.kubeconfig
+      ADMIN_PASSWORD   = self.triggers.admin_password
+      DELETE_USER_JSON = self.triggers.delete_user_json
+      CLEAR_ROLE_JSON  = self.triggers.clear_role_json
     }
     # Delete the binding's Solr user
     # Can't reuse containers because they are left in an unpredictable state after a single run
@@ -98,14 +108,14 @@ resource "null_resource" "manage_solr_user" {
         -o /dev/null \
         -w "%%{http_code}\n" \
         --user admin:$ADMIN_PASSWORD \
-        'http://${local.cloud_name}-solrcloud-common/solr/admin/authorization' \
+        'http://${self.triggers.cloud_name}-solrcloud-common/solr/admin/authorization' \
         -H 'Content-type:application/json' --data "$CLEAR_ROLE_JSON"
       kubectl --kubeconfig <(echo $KUBECONFIG | base64 -d) run temp2 --image=curlimages/curl --  \
         -s -f -L \
         -o /dev/null \
         -w "%%{http_code}\n" \
         --user admin:$ADMIN_PASSWORD \
-        'http://${local.cloud_name}-solrcloud-common/solr/admin/authentication' \
+        'http://${self.triggers.cloud_name}-solrcloud-common/solr/admin/authentication' \
         -H 'Content-type:application/json' --data "$DELETE_USER_JSON"
       sleep 10
       kubectl --kubeconfig <(echo $KUBECONFIG | base64 -d) delete pod temp2
