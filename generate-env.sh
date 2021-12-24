943010532e8d5c06e5ea8a6ae2cfa6b217ca0809
Nicholas Kumia <nicholas.kumia@gsa.gov>
2021-12-23 21:50:43 -0500

    revert: parameters to get KiND cluster to be functional

--- generate-env.sh
+++ generate-env.sh
@@ -6,16 +6,19 @@ CURRENT_CONTEXT=$(kubectl config current-context)
 CURRENT_CLUSTER=$(kubectl config view --raw -o json | jq -r '.contexts[]| select(.name | contains("'"${CURRENT_CONTEXT}"'"))  .context.cluster')
 CURRENT_USER=$(kubectl config view --raw -o json | jq -r '.contexts[]| select(.name | contains("'"${CURRENT_CONTEXT}"'"))  .context.user')
 SOLR_CLUSTER_CA_CERTIFICATE=$(kubectl config view --raw -o json | jq -r '.clusters[]| select(.name | contains("'"${CURRENT_CLUSTER}"'"))  .cluster["certificate-authority-data"]')
-SOLR_TOKEN=$(kubectl get secret "$( kubectl get serviceaccount default -n default -o json | jq -r '.secrets[0].name' )" -n default -o json | jq -r .data.token)
 SOLR_TOKEN=$(echo -n `kubectl config view --raw -o json | jq -r '.users[]| select(.name | contains("'"${CURRENT_USER}"'"))  .user["token"]'` | base64 -w 0)
 SOLR_SERVER=$(kubectl config view --raw -o json | jq -r '.clusters[]| select(.name | contains("'"${CURRENT_CLUSTER}"'"))  .cluster["server"]')
 
 SOLR_DOMAIN_NAME=${SOLR_DOMAIN_NAME:-ing.local.domain}
 
-if [ "${CURRENT_CLUSTER}" = "kind-datagov-broker-test" ] ; then
+if [ "${CURRENT_CLUSTER}" == *"kind-datagov-broker-test"* ]; then
     # If the test cluster is in KinD we need the CSB to use 
     # a control plane URL resolvable from inside the CSB Docker container
+    CURRENT_USER=kind-datagov-broker-test
+    CURRENT_CONTEXT=kind-datagov-broker-test
+    SOLR_SERVER=$(kind get kubeconfig --name=$(kind get clusters | grep datagov-broker-test) | grep server | cut -d ' ' -f 6-)
     SOLR_CP_SERVER=$(kind get kubeconfig --internal --name="$(kind get clusters | grep datagov-broker-test)" | grep server | cut -d ' ' -f 6-)
+    SOLR_TOKEN=$(echo -n `kubectl config view --raw -o json | jq -r '.users[]| select(.name | contains("'"${CURRENT_USER}"'"))  .user["client-key-data"]'` | base64 -w 0)
 else
     # Otherwise it's the same as the normal server control plane URL
     SOLR_CP_SERVER=${SOLR_SERVER}
