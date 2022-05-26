63a1a34bfcce65a248e32d78a9f7b72f9929e5fc
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-05-26 10:59:33 -0400

    fix: expose password for new binding
    This is a change that happened from terraform 0.13 to 0.15, since terraform was upgraded to 1.1, it's necessary to implement

--- terraform/bind/outputs.tf
+++ terraform/bind/outputs.tf
@@ -2,4 +2,4 @@
 output "uri" { value = var.solr_admin_url }
 output "domain" { value = var.solr_admin_url }
 output "username" { value = random_uuid.username.result }
-output "password" { value = random_password.password.result }
+output "password" { value = nonsensitive(random_password.password.result) }
