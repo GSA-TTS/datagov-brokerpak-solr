d856615a68d011d579b8861da46dc17e279a371d
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-07-15 18:02:33 -0400

    fix: there's only one certification to validate
    No need to be seem complex

--- terraform/ecs/provision/ssl.tf
+++ terraform/ecs/provision/ssl.tf
@@ -16,16 +16,14 @@ resource "aws_acm_certificate" "cert" {
 
 # Validate the certificate using DNS method (Parent Domain and subdomains)
 resource "aws_route53_record" "cert_validation" {
-  count   = 2
-  name    = tolist(aws_acm_certificate.cert.domain_validation_options)[count.index].resource_record_name
-  type    = tolist(aws_acm_certificate.cert.domain_validation_options)[count.index].resource_record_type
+  name    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
+  type    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_type
   zone_id = aws_route53_zone.cluster.id
-  records = [tolist(aws_acm_certificate.cert.domain_validation_options)[count.index].resource_record_value]
+  records = [tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_value]
   ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "cert" {
-  count                   = 2
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation[count.index].fqdn]
+  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
 }
