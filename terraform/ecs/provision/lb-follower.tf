3dd272f9d24b7e7621da43f9f0e702c4211501da
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-07-15 17:19:21 -0400

    new: add baseline lb configuration for followers
    - Create a new LB for all solr followers
    - All solr followers are in a single target group (This means that the solr follower that a request is sent to will be randomly selected)
    - There is no session stickiness for requests, so each new page reload or new request will be sent to a random solr follower
    - Solr Follower LB is using the same security groups as the Solr Leader LB

---
+++ terraform/ecs/provision/lb-follower.tf
@@ -0,0 +1,72 @@
+
+locals {
+  lb_name = substr(var.instance_name, 0, 29)
+}
+
+resource "aws_lb" "solr-follower" {
+  name               = "${local.lb_name}-follower-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.solr-lb-sg.id]
+  subnets            = module.vpc.public_subnets
+
+  enable_deletion_protection = false
+
+  # access_logs {
+  #   bucket  = aws_s3_bucket.lb_logs.bucket
+  #   prefix  = "test-lb"
+  #   enabled = true
+  # }
+
+  tags = {
+    name = "solr-${var.instance_name}"
+  }
+}
+
+resource "aws_lb_listener" "http_upgrade" {
+  load_balancer_arn = aws_lb.solr-follower.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https_response" {
+  load_balancer_arn = aws_lb.solr-follower.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  # TODO: create wildcard certificate
+  certificate_arn   = aws_acm_certificate.cert.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.solr-follower-target.id
+  }
+}
+
+resource "aws_lb_target_group" "solr-follower-target" {
+  name        = "${local.lb_name}-follower-tg"
+  port        = 8983
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = module.vpc.vpc_id
+
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    interval            = 30
+    path                = "/"
+    port                = 8983
+    protocol            = "HTTP"
+    timeout             = 10
+    matcher             = "200-399"
+  }
+}
