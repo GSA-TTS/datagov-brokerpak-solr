2fdd45be1b9ae2cb1e1d0e853dd5d239065f3db8
Fuhu Xia <fxia@reisystems.com>
2022-07-19 13:51:53 -0400

    modify command to enable solr replication

--- terraform/ecs/provision/follower.tf
+++ terraform/ecs/provision/follower.tf
@@ -25,6 +25,10 @@ resource "aws_ecs_task_definition" "solr-follower-no-efs" {
       command = ["/bin/bash", "-c", join(" ", [
         "df -h;",
         "cd /tmp; /usr/bin/wget -O solr_setup.sh ${var.setupFollowerLink}; /bin/bash solr_setup.sh;",
+        "mv /tmp/ckan_config/solrconfig_follower.xml /tmp/ckan_config/solrconfig.xml;",
+        "sed -i 's/SOLR_REPLICATION_LEADER_URL/https:\\/\\/${local.leader_domain}\\/solr\\/ckan\\/replication/g' /tmp/ckan_config/solrconfig.txt",
+        "sed -i 's/SOLR_REPLICATION_LEADER_USER/${random_uuid.username.result}/g' /tmp/ckan_config/solrconfig.txt",
+        "sed -i 's/SOLR_REPLICATION_LEADER_PASSWORD/${random_password.password.result}/g' /tmp/ckan_config/solrconfig.txt",
         "chown -R 8983:8983 /var/solr/data;",
         "cd -; su -c \"",
         "init-var-solr; precreate-core ckan /tmp/ckan_config; chown -R 8983:8983 /var/solr/data; solr-fg -m ${local.solrFollowerMemInG}g\" -m solr"
@@ -66,6 +70,10 @@ resource "aws_ecs_task_definition" "solr-follower" {
       essential = true
       command = ["/bin/bash", "-c", join(" ", [
         "cd /tmp; /usr/bin/wget -O solr_setup.sh ${var.setupFollowerLink}; /bin/bash solr_setup.sh;",
+        "mv /tmp/ckan_config/solrconfig_follower.xml /tmp/ckan_config/solrconfig.xml;",
+        "sed -i 's/SOLR_REPLICATION_LEADER_URL/https:\\/\\/${local.leader_domain}\\/solr\\/ckan\\/replication/g' /tmp/ckan_config/solrconfig.txt",
+        "sed -i 's/SOLR_REPLICATION_LEADER_USER/${random_uuid.username.result}/g' /tmp/ckan_config/solrconfig.txt",
+        "sed -i 's/SOLR_REPLICATION_LEADER_PASSWORD/${random_password.password.result}/g' /tmp/ckan_config/solrconfig.txt",
         "chown -R 8983:8983 /var/solr/data;",
         "cd -; su -c \"",
         "init-var-solr; precreate-core ckan /tmp/ckan_config; chown -R 8983:8983 /var/solr/data; solr-fg -m ${local.solrMemInG}g\" -m solr"
