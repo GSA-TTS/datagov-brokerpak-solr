0afc424681af3e66aaab98ce7507654b38e9d479
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-07-20 16:10:03 -0400

    fix: makefile-to-brokerpak quoting
    Make sure make reads the literal quotes within the json dictionary to pass to brokerpak;  If the quotes disappears, it is invalid brokerpak syntax

--- test.sh
+++ test.sh
@@ -20,6 +20,9 @@ if (jq ".[$i].description" examples.json | grep -q "(K8S)"); then
   INSTANCE_NAME=$INSTANCE_NAME K8S_CLOUD_PROVISION_PARAMS="$provision_params" K8S_CLOUD_BIND_PARAMS=$bind_params make k8s-demo-up
   INSTANCE_NAME=$INSTANCE_NAME make k8s-demo-down
 else
+  echo $INSTANCE_NAME
+  echo $provision_params
+  echo $bind_params
   INSTANCE_NAME=$INSTANCE_NAME ECS_CLOUD_PROVISION_PARAMS="$provision_params" ECS_CLOUD_BIND_PARAMS="$bind_params" make ecs-demo-up
   INSTANCE_NAME=$INSTANCE_NAME make ecs-demo-down
 fi
