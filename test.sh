d50f19e57f40a46dfdfc5b20c3d2f62ad1b3d6dc
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-07-20 16:42:01 -0400

    cleanup/fix: only call demo-up/demo-down
    Since ecs-/k8s-demo-up ecs-/k8s-demo-down doesn't exist anymore

--- test.sh
+++ test.sh
@@ -19,14 +19,7 @@ service_name=$( jq -r ".[$i].service_name" examples.json)
 service_id=$( jq -r ".[$i].service_id" examples.json)
 plan_id=$( jq -r ".[$i].plan_id" examples.json)
 
-if (jq ".[$i].description" examples.json | grep -q "(K8S)"); then
-  SERVICE_ID=$service_id PLAN_ID=$plan_id SERVICE_NAME=$service_name \
-    INSTANCE_NAME=$INSTANCE_NAME K8S_CLOUD_PROVISION_PARAMS="$provision_params" K8S_CLOUD_BIND_PARAMS=$bind_params make k8s-demo-up
-  SERVICE_ID=$service_id PLAN_ID=$plan_id SERVICE_NAME=$service_name \
-    INSTANCE_NAME=$INSTANCE_NAME make k8s-demo-down
-else
-  SERVICE_ID=$service_id PLAN_ID=$plan_id SERVICE_NAME=$service_name \
-    INSTANCE_NAME=$INSTANCE_NAME ECS_CLOUD_PROVISION_PARAMS="$provision_params" ECS_CLOUD_BIND_PARAMS="$bind_params" make ecs-demo-up
-  SERVICE_ID=$service_id PLAN_ID=$plan_id SERVICE_NAME=$service_name \
-    INSTANCE_NAME=$INSTANCE_NAME make ecs-demo-down
-fi
+SERVICE_ID=$service_id PLAN_ID=$plan_id SERVICE_NAME=$service_name \
+  INSTANCE_NAME=$INSTANCE_NAME ECS_CLOUD_PROVISION_PARAMS="$provision_params" ECS_CLOUD_BIND_PARAMS="$bind_params" make demo-up
+SERVICE_ID=$service_id PLAN_ID=$plan_id SERVICE_NAME=$service_name \
+  INSTANCE_NAME=$INSTANCE_NAME make demo-down
