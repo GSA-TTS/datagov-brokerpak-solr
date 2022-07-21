1047d376626fe55d1c2d15125404dead808aaef2
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-07-21 14:50:18 -0400

    new: update solrcloud terraform syntax

--- terraform/solrcloud/provision/providers.tf
+++ terraform/solrcloud/provision/providers.tf
@@ -2,7 +2,6 @@ provider "kubernetes" {
   host                   = var.server
   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
   token                  = base64decode(var.token)
-  version                = "~>2.7"
 }
 
 provider "helm" {
@@ -11,5 +10,4 @@ provider "helm" {
     cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
     token                  = base64decode(var.token)
   }
-  version = "~>2.4"
 }
