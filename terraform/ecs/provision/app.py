02ec90ceebc7815d774b99c04305c07c1f715688
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-08-31 12:49:58 -0400

    new: alert Slack
    - Fix restart cluster name param
    - Hardcode slack channel updates
    - Create custom layout for Solr error/recovery

--- terraform/ecs/provision/app.py
+++ terraform/ecs/provision/app.py
@@ -13,31 +13,89 @@ def handler(event, context):
     state = message_json['NewStateValue']
 
     # Parse/Restart ECS Service
+    service_dimensions = identifyCluster(message_json)
     if state == 'ALARM':
-        restartSolrECS(message_json)
+        restartSolrECS(message_json, service_dimensions['ClusterName'], service_dimensions['ServiceName'])
+        notifySlack(message_json, service_dimensions['ClusterName'], service_dimensions['ServiceName'])
     elif state == 'OK':
-        pass
+        notifySlack(message_json, service_dimensions['ClusterName'], service_dimensions['ServiceName'])
 
     return message
 
-def restartSolrECS(message_json):
+
+def identifyCluster(event_info):
+    service_dimensions = {}
+    for dim in event_info['Trigger']['Dimensions']:
+        service_dimensions[dim['name']] = dim['value']
+    return service_dimensions
+
+
+def restartSolrECS(message_json, cluster_name, service_name):
     '''
     Reference: https://github.com/s7anley/aws-ecs-service-stop-lambda/blob/master/main.py
     '''
-    service_dimensions = {}
-    for dim in message_json['Trigger']['Dimensions']:
-        service_dimensions[dim['name']] = dim['value']
     service_region = message_json['AlarmArn'].split(':')[3]
 
     client = boto3.client("ecs", region_name=service_region)
     response = client.list_tasks(
-        cluster=service_dimensions['ClusterName'],
-        family="%s-service" % (service_dimensions['ServiceName']))
+        cluster=cluster_name,
+        family="%s-service" % (service_name))
     tasks = response.get('taskArns', [])
     print("Service is running {0} underlying tasks".format(len(tasks)))
 
     for task in tasks:
         print("Stopping tasks {0}".format(tasks))
-        client.stop_task(cluster=cluster, task=task)
+        client.stop_task(cluster=cluster_name, task=task)
 
     print("Completed service restart")
+
+
+def notifySlack(event_info, cluster, service):
+    from slack_sdk.webhook import WebhookClient
+    url = "https://hooks.slack.com/services/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"
+    webhook = WebhookClient(url)
+
+    important_data = ["Alarm Name", "New State Value", "New State Reason", "State Change Time"]
+
+    emoji = "😨" if event_info["NewStateValue"] == "ALARM" else "😐"
+
+    response = webhook.send(blocks=[
+        {
+            "type": "divider"
+        },
+        {
+            "type": "context",
+            "elements": [
+                {
+                    "type": "image",
+                    "image_url": "https://s3-us-gov-west-1.amazonaws.com/cg-0817d6e3-93c4-4de8-8b32-da6919464e61/solr.png",
+                    "alt_text": "Solr Icon"
+                },
+                {
+                    "type": "mrkdwn",
+                    "text": ":::ALERT::: *%s/%s* has experienced an event. " % (service['ClusterName'], service['ServiceName']) + emoji
+                }
+            ]
+        },
+        {
+            "type": "divider"
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "".join(["*%s*:\n%s\n\n" % (key, event_info[key.replace(" ", "")]) for key in important_data])
+            },
+            "accessory": {
+                "type": "image",
+		"image_url": "https://s3-us-gov-west-1.amazonaws.com/cg-0817d6e3-93c4-4de8-8b32-da6919464e61/solr.png",
+                "alt_text": "Solr Alert"
+            }
+        },
+        {
+            "type": "divider"
+        }
+    ])
+
+    assert response.status_code == 200
+    assert response.body == "ok"
