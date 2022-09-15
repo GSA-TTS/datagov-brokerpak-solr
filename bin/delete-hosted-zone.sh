5a91d70a8152a8b2d1d2e23ec026309805368470
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-09-15 15:33:47 -0400

    refactor: fancify
    Add verbose mode to hide aws outputs and only show them if verbose; continue (don't exit) if first domain is not valid

--- bin/delete-hosted-zone.sh
+++ bin/delete-hosted-zone.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Borrowed/Extended from: https://alestic.com/2016/09/aws-route53-wipe-hosted-zone/
+VERBOSE=false
 
 for domain_to_delete in "$@"; do
   # Pass in the domain to delete based on input list
@@ -12,14 +13,16 @@ for domain_to_delete in "$@"; do
       --output text \
       --query 'HostedZones[?Name==`'$domain_to_delete'.`].Id'
   )
-  echo hosted_zone_id=$hosted_zone_id
 
   if [ -z $hosted_zone_id ]; then
     echo "Calculating...No such domain...no work to do! YAY!"
-    exit 0
+    continue
   fi
 
+  echo hosted_zone_id=$hosted_zone_id
+
   # Delete all Records from the domain
+  echo -n "Deleting records..."
   aws route53 list-resource-record-sets \
     --hosted-zone-id $hosted_zone_id |
   jq -c '.ResourceRecordSets[]' |
@@ -31,22 +34,32 @@ for domain_to_delete in "$@"; do
         --change-batch '{"Changes":[{"Action":"DELETE","ResourceRecordSet":
             '"$resourcerecordset"'
           }]}' \
-        --output text --query 'ChangeInfo.Id'
+        --output text --query 'ChangeInfo.Id' > /tmp/delete-route53
+      $VERBOSE && cat /tmp/delete-route53
     fi
   done
+  echo "ok"
 
-  # Disable DNSSEC
+  echo -n "Disabling DNSSEC..."
   # https://docs.aws.amazon.com/cli/latest/reference/route53/disable-hosted-zone-dnssec.html
-  aws route53 disable-hosted-zone-dnssec --hosted-zone-id $hosted_zone_id
+  aws route53 disable-hosted-zone-dnssec --hosted-zone-id $hosted_zone_id > /tmp/delete-route53
+  echo "ok"
+  $VERBOSE && cat /tmp/delete-route53
 
-  # Deactivate KSK
+  echo -n "Deactivating KSK..."
   # https://docs.aws.amazon.com/cli/latest/reference/route53/deactivate-key-signing-key.html
-  aws route53 deactivate-key-signing-key --hosted-zone-id $hosted_zone_id --name $domain_to_delete
+  aws route53 deactivate-key-signing-key --hosted-zone-id $hosted_zone_id --name $domain_to_delete > /tmp/delete-route53
+  echo "ok"
+  $VERBOSE && cat /tmp/delete-route53
 
-  # Delete KSK
+  echo -n "Deleting KSK..."
   # https://docs.aws.amazon.com/cli/latest/reference/route53/delete-key-signing-key.html
-  aws route53 delete-key-signing-key --hosted-zone-id $hosted_zone_id --name $domain_to_delete
+  aws route53 delete-key-signing-key --hosted-zone-id $hosted_zone_id --name $domain_to_delete > /tmp/delete-route53
+  echo "ok"
+  $VERBOSE && cat /tmp/delete-route53
 
-  # Delete domain zone
-  aws route53 delete-hosted-zone --id $hosted_zone_id
+  echo -n "Deleting zone..."
+  aws route53 delete-hosted-zone --id $hosted_zone_id > /tmp/delete-route53
+  echo "ok"
+  $VERBOSE && cat /tmp/delete-route53
 done
